9cabb15f784db71346e98b87f593438d0ff7a5f4
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2024-05-13 15:44:39 +0200

    connexion db et info livres

--- Code/p-app-lecture/Class/Livre.cs
+++ Code/p-app-lecture/Class/Livre.cs
@@ -8,8 +8,12 @@ namespace p_app_lecture.Class
 {
     public class Book
     {
+        public int Id { get; set; }
         public string Title { get; set; }
-        public string Author { get; set; }
-        public string CoverImagePath { get; set; }
+        public byte[] EpubFile { get; set; }
+        public string Summary { get; set; }
+        public int YearPublished { get; set; }
+        public byte[] CoverImage { get; set; }
+        public DateTime DateAdded { get; set; }
     }
 }
--- Code/p-app-lecture/MainPage.xaml.cs
+++ Code/p-app-lecture/MainPage.xaml.cs
@@ -6,18 +6,61 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using VersOne.Epub;
-using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Data;
 
 namespace p_app_lecture
 {
     public partial class MainPage : ContentPage
     {
         string connectionString = "server=localhost;user=root;database=db_livres;port=3306;password=root;";
-
-
+        public ObservableCollection<Book> Books = new ObservableCollection<Book>();
         public MainPage()
         {
             InitializeComponent();
+            Books = new ObservableCollection<Book>();
+            GetLivresFromDB();
+            BindingContext = this;
+        }
+
+        private void GetLivresFromDB()
+        {
+            try
+            {
+                
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Définir la commande SQL pour récupérer les informations sur les livres
+                    string query = "SELECT * FROM livres";
+                    MySqlCommand command = new MySqlCommand(query, connection);
+
+                    // Exécuter la commande et récupérer les résultats
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            
+                            Book book = new Book();
+                            Debug.WriteLine($"Nombre de livres récupérés : {Books.Count}");
+                            
+                            book.Id = reader.GetInt32("idLivre");
+                            book.Title = reader.GetString("titre");
+                            //book.EpubFile = 
+                            book.Summary = reader.GetString("extrait");
+                            book.YearPublished = reader.GetInt32("anneeEdition");
+                            //book.CoverImage
+                            Books.Add(book);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
